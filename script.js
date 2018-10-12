62f4c8b66cb5e83d3ac8c38748a41ea6d4ecd844
JadeTycoon <anhardy381@gmail.com>
2018-10-12 12:24:49 -0400

    Fixed switch statement

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let pile2 = 3;
 let pile3 = 3;
 
 piles = [pile1, pile2, pile3];
+namesOfPiles = ['pile1', 'pile2', 'pile3'];
 gameOrder = [];
 
 function initialize(){
@@ -34,12 +35,16 @@ function main(ele){
             
             case '3':
                 if (piles.indexOf(pile3) != -1){
+                    console.log(ele);
+                    console.log(pile3);
                     return pile3;
                 } else {
                     return valid;
                 };
             
-            default: return valid;
+            default: 
+                console.log(ele);
+                return valid;
     
             };
     }
@@ -48,25 +53,28 @@ function main(ele){
 
     // validator
     pile = pile(ele)
+    console.log(pile)
     if (pile){
-        pile--;
-        if (pile < 1){
-            piles.splice(piles.indexOf[pile], 1)
-        } // deletes from list if less than 1
         let pileE = (pile) => {
-            console.log("hi")
+            console.log(pile)
             // doesn't work past this line
-            switch (pile){
-                case pile = pile1: return pile1E;
-                case pile = pile2: return pile2E;
-                case pile = pile3: return pile3E;
+            switch (ele){
+                case '1': return pile1E;
+                case '2': return pile2E;
+                case '3': return pile3E;
                 default: return "hey";
             };
         };
 
         // switch statement return undefined, doesn't seem cases are working
-        pileEL = pileE(pile)
+        pileEL = pileE(ele)
         console.log(pileEL)
+        
+        pile--;
+        if (pile < 1){
+            piles.splice(piles.indexOf[pile], 1);
+        }; // deletes from list if less than 1
+
         pileEL.innerHTMl=pile;
 
         gameOrder.push('user');
@@ -103,14 +111,14 @@ function foo(){
     // Chooses random pile from piles list. This avoids the need to check the pile, because
     // everything in the list is at least 1. Checkpile is only used after, so it can modify
     // the list.
-    let randpile = piles[Math.floor(Math.random()*piles.length)];
+    let randpile = namesOfPiles[Math.floor(Math.random()*namesOfPiles.length)];
 
     // Identifies what element is currently being used
     let pileE = (randpile) => {
         switch (randpile){
-            case randpile == pile1: return pile1E;
-            case randpile == pile2: return pile2E;
-            case randpile == pile3: return pile3E;
+            case 'pile1': return pile1E;
+            case 'pile2': return pile2E;
+            case 'pile3': return pile3E;
         };
     };
     randpile--;
